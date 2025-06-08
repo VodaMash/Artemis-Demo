9264cd2da0e12cc75e66eaded8372f95d2e44a8c
VodaMash <ahmad.mashaal2@vodafone.com>
2025-06-08 04:42:40 +0300

    nit

--- src/main/java/com/mash/kratos/KratosApplication.java
+++ src/main/java/com/mash/kratos/KratosApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class KratosApplication {
-
   public static void main(String[] args) {
     SpringApplication.run(KratosApplication.class, args);
   }
